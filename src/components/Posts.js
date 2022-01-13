e6fedfb82e37d937fe140b8a36b6b9b8015b2a37
Walles Mufonyongo <Walleskotino@gmail.com>
2022-01-13 10:27:33 +0300

    fetching data with REST API

---
+++ src/components/Posts.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Posts() {
+  const [post, getPost] = useState([])
+
+  const API = 'https://jsonplaceholder.typicode.com/posts';
+
+  const fetchPost = () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+        getPost(res)
+      })
+  }
+
+  useEffect(() => {
+    fetchPost()
+  }, [])
+
+  return (
+    <>
+    <div>
+      <h2>React Fetch Data with REST API Example</h2>
+      </div>
+      <ul>
+        {post.map((item, i) => {
+          return <li key={i}>{item.title}</li>
+        })}
+      </ul>
+    </>
+  )
+}
